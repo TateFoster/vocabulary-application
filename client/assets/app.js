e6510e2cada148577701ed315fbffbf77f157596
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2023-01-24 14:19:43 -0600

    working on first instance of VocabApp

---
+++ client/assets/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const VocabApp = () => {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+            <Text> Vocabulary App</Text>
+        </View>
+    )
+};
+export default VocabApp;
