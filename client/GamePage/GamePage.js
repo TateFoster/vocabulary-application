dc1cdbd505d2f8eb6e65bbc16ac86f4e60e5661e
Dragon Lore <tate.j.foster@gmail.com>
2023-01-24 13:57:30 -0700

    setting up GamePage section with shell and starting of word sets

---
+++ client/GamePage/GamePage.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function GamePage() {
+	const wordGroups = {
+		setOne: {
+			groupOne: ["big", "large", "huge"],
+			groupTwo: ["small", "tiny", "minute"],
+		},
+	};
+
+	return (
+		<View>
+			<Text>Here is place for game</Text>
+		</View>
+	);
+}
