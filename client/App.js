cc567708f0644b21a308f3b609e29607769967c6
Dragon Lore <tate.j.foster@gmail.com>
2023-01-24 10:58:02 -0700

    tested app with MainArea, will start setting up word bank next

--- client/App.js
+++ client/App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MainArea from "./MainArea/MainArea";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	let engage = false;
+
+	if (engage) {
+		return <MainArea />;
+	} else {
+		return (
+			<View style={styles.container}>
+				<Text>Open up App.js to start working on your app! Testing</Text>
+
+				<StatusBar style="auto" />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
