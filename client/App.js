7bb66f6f543b5907fe3bb7f8ded71429fcfcd194
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2023-01-25 13:18:58 -0600

    flex containers for instances

--- client/App.js
+++ client/App.js
@@ -3,8 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={[styles.container,
+    { flexDirection: 'column' },
+
+    ]}>
+      <Text>VocabApp!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,8 +15,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 3,
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- client/components/index.js
+++ client/components/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// importing bootstrap for styling 
 import 'bootswtrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
