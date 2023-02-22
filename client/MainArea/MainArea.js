26293461e9f8cac2ffd44919d73670313ecaaa49
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2023-02-21 19:27:04 -0600

    removing heading from Typography for render

--- client/MainArea/MainArea.js
+++ client/MainArea/MainArea.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import React from 'react';
 import { Text, View } from 'react-native';
 import { Header } from './Header';
-import { heading } from './Typography';
+// import { heading } from './Typography';
 
 export default function MainArea() {
 	return (
@@ -11,22 +11,22 @@ export default function MainArea() {
 			<Text>Welcome to VOCABAPP</Text>
 			<View>
 				<Header title="Welcome to React Native" />
-				<Text style={heading}>Game One</Text>
+				<Text>Game One</Text>
 				<Text>
 					Edit App.js to change this screen and turn it
 					into your app.
 				</Text>
-				<Text style={heading}>Game Two</Text>
+				<Text>Game Two</Text>
 				<Text>
 					Press Cmd + R inside the simulator to reload
 					your app’s code.
 				</Text>
-				<Text style={heading}>Game Three</Text>
+				<Text>Game Three</Text>
 				<Text>
 					Press Cmd + M or Shake your device to open the
 					React Native Debug Menu.
 				</Text>
-				<Text style={heading}>Help</Text>
+				<Text>Help</Text>
 				<Text>
 					Read the docs to discover what to do next:
 				</Text>
