1c13c0a54f9fe64b1b8b40d91a0b594110d75c41
Sabrina Carrion <SabrinaCarrion43@gmail.com>
2023-02-24 09:33:44 -0600

    testing Main CSS UX design for three games

--- client/MainArea/MainArea.js
+++ client/MainArea/MainArea.js
@@ -1,31 +1,38 @@
 import { StyleSheet, Text, View } from "react-native";
+import './MainArea.css';
 
 import React from 'react';
 import { Text, View } from 'react-native';
 import { Header } from './Header';
-// import { heading } from './Typography';
+
 
 export default function MainArea() {
 	return (
 		<View>
 			<Text>Welcome to VOCABAPP</Text>
 			<View>
-				<Header title="Welcome to React Native" />
-				<Text>Game One</Text>
-				<Text>
-					Edit App.js to change this screen and turn it
-					into your app.
-				</Text>
-				<Text>Game Two</Text>
-				<Text>
-					Press Cmd + R inside the simulator to reload
-					your app’s code.
-				</Text>
-				<Text>Game Three</Text>
-				<Text>
-					Press Cmd + M or Shake your device to open the
-					React Native Debug Menu.
-				</Text>
+				<section className="gameOne">
+					<Header title="Welcome to the VocabApp Game" />
+				</section>
+
+				<div claassName="firstInstance">
+					<Text>
+						Edit App.js to change this screen and turn it
+						into your app.
+					</Text>
+				</div>
+				<section className="gameTwo">
+					<Text>Game Two</Text>
+					<Text>
+						Press Cmd + R inside the simulator to reload
+						your app’s code.
+					</Text>
+					<Text>Game Three</Text>
+					<Text>
+						Press Cmd + M or Shake your device to open the
+						React Native Debug Menu.
+					</Text>
+				</section>
 				<Text>Help</Text>
 				<Text>
 					Read the docs to discover what to do next:
--- client/components/primaryGame.js
+++ client/components/primaryGame.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { TextInput, Text, StyleSheet, View, } from 'react-native';
+import { View, } from 'react-native';
+
 
 const Welcome = () => {
     const title = "Welcome to your first challenge!"
     return (
         <View
+            header={title}
             style={[styles.container, {
                 flexDirection: 'column',
             },
             ]}>
             <div className='sidebar' />
-            <Text> {title}
-            </Text>
-            <Button color="steelblue" shadowSize={1}>
+
+            <Button color="steelblue" variant="primary" shadowSize={1}>
                 Click Me
             </Button>
 
