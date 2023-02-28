28f95907f912f433cd84519f563f5ac5d2e49117
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2023-02-27 20:28:36 -0600

    added a test button and some styling

--- client/MainArea/MainArea.js
+++ client/MainArea/MainArea.js
@@ -1,32 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
+// import { StyleSheet, Text, View } from "react-native";
 
 import React from 'react';
-import { Text, View } from 'react-native';
-import { Header } from './Header';
-import { heading } from './Typography';
+import { 
+	Text,
+	 View,
+	Button } from 'react-native';
+// import { Header } from './Header';
+// import { heading } from './Typography';
 
 export default function MainArea() {
 	return (
 		<View>
-			<Text>Welcome to VOCABAPP</Text>
+			<Text style={styles.header}>Word Wizard</Text>
 			<View>
-				<Header title="Welcome to React Native" />
-				<Text style={heading}>Game One</Text>
+				{/* <Header title="Welcome to React Native" /> */}
+				{/* <Text style={heading}>Game One</Text> */}
+				<Button style={styles.mainButton}
+				title="Next Lesson"
+				color="#000" />
 				<Text>
 					Edit App.js to change this screen and turn it
 					into your app.
 				</Text>
-				<Text style={heading}>Game Two</Text>
+				{/* <Text style={heading}>Game Two</Text> */}
 				<Text>
 					Press Cmd + R inside the simulator to reload
 					your app’s code.
 				</Text>
-				<Text style={heading}>Game Three</Text>
+				{/* <Text style={heading}>Game Three</Text> */}
 				<Text>
 					Press Cmd + M or Shake your device to open the
 					React Native Debug Menu.
 				</Text>
-				<Text style={heading}>Help</Text>
+				{/* <Text style={heading}>Help</Text> */}
 				<Text>
 					Read the docs to discover what to do next:
 				</Text>
@@ -35,4 +41,14 @@ export default function MainArea() {
 
 		</View>
 	);
+}
+
+
+const styles = {
+	header: {
+		fontSize: "7rem"
+	},
+	mainButton: {
+		width: "80%"
+	}
 }
